89ce5b2c2fcbba6eedb7e8fe1c5f7a644dc8b88e
Aderemi Okeowo <aderemiokeowo@MacBook-Pro-50.local>
2018-09-12 14:38:49 +0100

    Updated Simple Android App

--- FirstAndroidApp/app/src/main/java/org/retani/firstandroidapp/MainActivity.java
+++ FirstAndroidApp/app/src/main/java/org/retani/firstandroidapp/MainActivity.java
@@ -1,10 +1,14 @@
 package org.retani.firstandroidapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String MESSAGE = "org.retani.message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +17,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendMessage(View view){
+        Intent intent = new Intent(this,DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(MainActivity.MESSAGE,message);
+        startActivity(intent);
+
 
     }
 }
